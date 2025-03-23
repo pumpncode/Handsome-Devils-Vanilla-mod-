c8fbb83cde96f080a2b969ed1687293cb9556ec2
Xavier Torrescano <xmtrocks@gmail.com>
2025-03-23 09:44:27 -0700

    rework perfectionist into a lovely patch

--- jokers/perfectionist.lua
+++ jokers/perfectionist.lua
@@ -1,24 +1,3 @@
-local set_ability_ref = Card.set_ability
-Card.set_ability = function(self, center, initial, delay_sprites)
-    local ret = set_ability_ref(self, center, initial, delay_sprites)
-    if self.ability and self.ability.set == "Enhanced" and center.set == "Enhanced" and self.area ~= G.deck then
-        if G.jokers.cards then for i = 1, #G.jokers.cards do
-            if G.jokers.cards[i].config.center.key == "j_hnds_perfectionist" then
-                self.ability.perma_mult = self.ability.perma_mult or 0
-                self.ability.perma_mult = self.ability.perma_mult + G.P_CENTERS["j_hnds_perfectionist"].config.extra.mult
-                card_eval_status_text(self, 'extra', nil, nil, nil, {message = localize('k_upgrade_ex'), colour = G.C.MULT})
-                G.E_MANAGER:add_event(Event({
-                    func = function()
-                        G.jokers.cards[i]:juice_up(0.6)
-                    return true end
-                }))
-            end
-        end
-        end
-    end
-    return ret
-end
-
 SMODS.Joker {
     key = "perfectionist",
     config = {
